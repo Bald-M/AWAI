df329b27be73258ab933572edfcd94fde6487e52
zihan <2411663991@qq.com>
2024-05-21 00:55:49 +1200

    add setting fragment

--- app/src/main/java/com/example/awai/CallingFragment.kt
+++ app/src/main/java/com/example/awai/CallingFragment.kt
@@ -3,6 +3,7 @@ package com.example.awai
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -10,18 +11,27 @@ import androidx.navigation.fragment.findNavController
 class CallingFragment : Fragment(R.layout.fragment_calling) {
     // 3 sec = 3000
     private var countdownTime : Long = 3000
+    private var countdownTimer: CountDownTimer? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         var countdownTV = view.findViewById<TextView>(R.id.countdownTV)
+        var cancelBtn = view.findViewById<Button>(R.id.cancelBtn)
+
+        cancelBtn.setOnClickListener{
+            // When tap "I am safe button", countdown must be canceled
+            countdownTimer?.cancel()
+            findNavController().navigate(R.id.action_callingFragment_to_homeFragment)
+            // Need to fix when bot tab changed, countdown must be canceled as well
+        }
 
         startCountdown(countdownTV)
 
     }
 
     fun startCountdown(countdownTV: TextView) {
-        var countdownTimer = object : CountDownTimer(countdownTime, 1000) {
+        countdownTimer = object : CountDownTimer(countdownTime, 1000) {
             override fun onTick(millisUntilFinished : Long) {
                 val secondsRemaining = (millisUntilFinished / 1000).toInt()
                 countdownTV.text = secondsRemaining.toString()
@@ -32,7 +42,7 @@ class CallingFragment : Fragment(R.layout.fragment_calling) {
                  findNavController().navigate(R.id.action_callingFragment_to_notificationFragment)
             }
         }
-
+        // Start countdown
         countdownTimer?.start()
     }
 }
--- app/src/main/java/com/example/awai/MainActivity.kt
+++ app/src/main/java/com/example/awai/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.awai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        val navHostFragment = findViewById<FragmentContainerView>(R.id.nav_host_fragment)
+
+        // switch tab
+        bottomNav.setOnItemSelectedListener {
+            item -> when(item.itemId) {
+
+                R.id.homeFragment -> {
+
+                    navHostFragment.findNavController().navigate(R.id.homeFragment)
+                    true
+                }
+
+                // Setting Fragment
+                R.id.settingFragment -> {
+                    navHostFragment.findNavController().navigate(R.id.settingFragment)
+                    true
+                }
+
+                else -> false
+
+            }
+        }
     }
 }
