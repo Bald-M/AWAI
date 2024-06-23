af376d9b07a6c51bd507ee6028537a61da7e68b4
zihan <2411663991@qq.com>
2024-06-23 20:39:39 +1200

    commit

---
+++ app/src/main/java/com/example/awai/Contact.kt
@@ -0,0 +1,9 @@
+package com.example.awai
+
+data class Contact(
+    val id: Long,
+    val name: String,
+    val email: String,
+    val address: String,
+    val number: Int
+)
--- app/src/main/java/com/example/awai/ContactManagementFragment.kt
+++ app/src/main/java/com/example/awai/ContactManagementFragment.kt
@@ -1,11 +1,18 @@
 package com.example.awai
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class ContactManagementFragment : Fragment(R.layout.fragment_contact_management) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val addBtn = view.findViewById<Button>(R.id.addBtn)
+        addBtn.setOnClickListener{
+            Log.i("test","test")
+        }
     }
 }
